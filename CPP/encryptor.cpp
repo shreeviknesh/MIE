605bd1415903f2468d1b96566585691236b2045b
shreeviknesh <luckyboy1998@gmail.com>
2020-04-28 17:47:35 +0530

    Migrated from C to CPP

--- CPP/encryptor.cpp
+++ CPP/encryptor.cpp
@@ -5,33 +5,56 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb-library/stb_image_write.h"
 
+// Standard libraries
+#include <string>
+#include <iostream>
+
 typedef uint8_t BYTE;
 
 //FUNCTION HEADERS
-void encrypt(char INPUT_FILENAME[], char OUTPUT_FILENAME[]);
-void decrypt(char INPUT_FILENAME[], char OUTPUT_FILENAME[]);
+void encrypt(std::string INPUT_FILENAME, std::string OUTPUT_FILENAME);
+void decrypt(std::string INPUT_FILENAME, std::string OUTPUT_FILENAME);
 BYTE gf_inverse(BYTE x);
 
 //MAIN DRIVER PROGRAM
-int main() {
-	encrypt("test.jpg", "test-o.jpg");
-	decrypt("test-o.jpg", "test-i.jpg");
-    return 0;
+int main(int argc, char** argv) {
+	std::string usage = "Usage: <FILE.exe> [[-E/-e/encrypt]/[-D/-d/decrypt]] <INPUT_FILENAME> <OUTPUT_FILENAME>";
+    if(argc < 4) {
+    	std::cout << "Invalid Command Line Arguments" << std::endl;
+    	std::cout << usage << std::endl;
+    	return 0;
+    }
+
+    std::string func = argv[1];
+    std::string in_file = argv[2];
+    std::string out_file = argv[3];
+
+    if(func == "-E" || func == "-e" || func == "encrypt") {
+    	encrypt(in_file, out_file);
+    }
+    else if(func == "-D" || func == "-d" || func == "decrypt") {
+    	decrypt(in_file, out_file);
+	} 
+	else {
+		std::cout << "Invalid Option" << std::endl;
+		std::cout << usage << std::endl;
+	}
+
+	return 0;
 }
 
 //ALL FUNCTION DEFINITIONS
-void encrypt(char INPUT_FILENAME[], char OUTPUT_FILENAME[]) {
+void encrypt(std::string INPUT_FILENAME, std::string OUTPUT_FILENAME) {
     int width, height, comp;
     unsigned long long i, j, index;
     int val;
 
-    BYTE* rgb_image = stbi_load(INPUT_FILENAME, &width, &height, &comp, 0);
+    BYTE* rgb_image = stbi_load(INPUT_FILENAME.c_str(), &width, &height, &comp, 0);
 
     for (i = 0; i < width; i++) {
         for (j = 0; j < height * comp; j++) {
             index = i + j * width;
             val = rgb_image[index];
-
             val = gf_inverse(val);
 
 			if(i % 2 == 0){
@@ -42,21 +65,20 @@ void encrypt(char INPUT_FILENAME[], char OUTPUT_FILENAME[]) {
 			}
 
 			val = val % 256;
-
 			rgb_image[index] = val;
         }
     }
 
-    stbi_write_png(OUTPUT_FILENAME, width, height, comp, rgb_image, width * comp);
+    stbi_write_png(OUTPUT_FILENAME.c_str(), width, height, comp, rgb_image, width * comp);
     stbi_image_free(rgb_image);
 }
 
-void decrypt(char INPUT_FILENAME[], char OUTPUT_FILENAME[]) {
+void decrypt(std::string INPUT_FILENAME, std::string OUTPUT_FILENAME) {
     int width, height, comp;
     unsigned long long i, j, index;
     int val;
 
-    BYTE* rgb_image = stbi_load(INPUT_FILENAME, &width, &height, &comp, 0);
+    BYTE* rgb_image = stbi_load(INPUT_FILENAME.c_str(), &width, &height, &comp, 0);
 
     for (i = 0; i < width; i++) {
         for (j = 0; j < height * comp; j++) {
@@ -76,7 +98,7 @@ void decrypt(char INPUT_FILENAME[], char OUTPUT_FILENAME[]) {
         }
     }
 
-    stbi_write_png(OUTPUT_FILENAME, width, height, comp, rgb_image, width * comp);
+    stbi_write_png(OUTPUT_FILENAME.c_str(), width, height, comp, rgb_image, width * comp);
     stbi_image_free(rgb_image);
 }
 
@@ -87,6 +109,7 @@ int bitlength(int x) {
             return y;
         }
     }
+    return y;
 }
 
 BYTE gf_inverse(BYTE x) {
